3cfe6c1137849a525fa315f6a324ad1117b5913e
Miguel Coronado <miguelcb84@gmail.com>
2013-09-19 02:21:07 +0200

    Concluida el procesado de ids y nombres para cambiar las referencias por titulo a referencias por uri

--- ifttt/pipelines.py
+++ ifttt/pipelines.py
@@ -5,22 +5,66 @@
 from ifttt.items import ChannelItem, ActionItem, EventItem, RecipeItem
 from scrapy import log
 from scrapy.item import Item
+import pickle
 
 
 class IdRegistryPipeline(object):
-    ''' '''
-    
+    '''  Pipeline that is responsible of gathering the URI of resources 
+         scraped, creating a dict that maps them to their title, and using
+         that dict to replace the references among objects made to the title 
+         with references made to the URI.
+         
+         Spiders with 'channel' in its name are supposed to fill the id map and
+         save it to disk. For each Channel, Event or Action item they find, the
+         field with name 'title' is stored as key to the URI of the item.
+         
+         For instance, consider the item shown:
+         >>> print item['title']
+         Gmail
+         >>> print item['id']
+         https://ifttt.com/Gmail
+         
+         This pipeline will add the following entry to the dict:
+         >>> print id_ds['Gmail']
+         https://ifttt.com/Gmail
+         
+         It is important to point out that it also performs the search crawling 
+         through the fields of the items.
+         
+         Spiders with 'rule' in its name are supposed to use the ids collected 
+         and replace the references to instances made by title with references
+         by URI.
+         
+         For instance. The field 'event_channel' stores the reference to the 
+         channel whose event triggers the rule considered. It should be a URI.
+         >>> print item['event_channel'] 
+         'Gmail'
+         
+         After pipeline processing:
+         >>> print item['event_channel'] 
+         'https://ifttt.com/Gmail'          
+    '''
+    ds_filename= 'id_datastore'
     REPLACE_FIELDS = ['action', 'action_channel', 'event', 'event_channel']
-    
     id_ds = {}    
     
     def __init__(self, *args, **kwargs):
-        # read from file
-        pass
+        try:
+            with open(self.ds_filename, 'rb') as f:
+                self.id_ds = pickle.load(f)
+            f.close()
+        except IOError:
+            pass
+    
+    def open_spider(self, spider):
+        if 'channel' in spider.name:
+            self.id_ds = {}
+        elif 'rule' in spider.name and not self.id_ds:
+            log.msg("No ids loaded", level=log.WARNING)
+            
     
     def process_item(self, item, spider):
         ''' '''
-
         if isinstance(item, RecipeItem):
             # Make substitutions
             for field in self.REPLACE_FIELDS:
@@ -28,9 +72,25 @@ class IdRegistryPipeline(object):
                 if replacement:
                     log.msg("[IdRegistryPipeline] Field " + field + " replaced by:" + replacement, level=log.DEBUG)
                     item[field] = replacement
-                
+        else:
+            self._register_item(item)
+        return item
+    
+    
+    def close_spider(self, spider):
+        # save to file
+        if 'channel' in spider.name:
+            with open(self.ds_filename, 'wt') as f:
+                pickle.dump(self.id_ds, f)
+            f.close()
+        elif 'rule' in spider.name:
+            pass
+
+
+    def _register_item(self, item):
         
-        elif type(item) in [ChannelItem, EventItem, ActionItem]:
+        log.msg("Register_item:" + str(item))
+        if type(item) in [ChannelItem, EventItem, ActionItem]:
             # Register new ids
             if not item['title']:
                 log.msg("[IdRegistryPipeline] The item consideren has no title field:" + str(item), level=log.WARNING)
@@ -46,18 +106,24 @@ class IdRegistryPipeline(object):
                         "> has been associated to the id:" + str(item['id']), 
                         level = log.DEBUG)
                 self.id_ds[item['title']] = item['id']
-        
-        return item
-    
-    
-    def close_spider(self, spider):
-        # save to file
-        if spider.name == 'channel':
-            log.msg('Event Ids:')
-            log.msg(str(self.event_ds))
-            log.msg('Action Ids')
-            log.msg(str(self.action_ds))
-                
+            
+            
+        # Check for field with items as value
+        # Here, we consider all items not only those with replacements
+        if isinstance(item, Item):
+            log.msg("Fields:" + str(item.keys()), level=log.DEBUG)
+            for field in item.keys():
+                val = item[field]
+                log.msg("Field: " + str(field) + ">>" + str(val), level=log.DEBUG)
+                if isinstance(val, Item):
+                    log.msg("It is item:" + str(val))
+                    self._register_item(val)
+                elif isinstance(val, list):
+                    log.msg("It is a list:" + str(val), level=log.DEBUG)
+                    for i in val:
+                        log.msg("List member: " + str(i), level=log.DEBUG)
+                        self._register_item(i)
+                    
 
 class RemoveEmptyItemsPipeline(object):
     ''' This pipeline removes all those items scraped where all fields are 
@@ -104,41 +170,3 @@ class RemoveEmptyItemsPipeline(object):
             
         return ret
 
-
-
-class FileExporterPipeline(object):
-
-    def __init__(self):
-        log.msg("[FileExporterPipeline] Initialize...", level=log.DEBUG)
-        pass
-
-    def process_item(self, item, spider):
-        log.msg("[FileExporterPipeline] Process Item:<" + str(item) + "> found by spider:<" + spider.name + ">", level=log.DEBUG)
-        filename = 'scraped_data/channels/' + item['title'] + '.rdf'
-        with open(filename, 'w') as f:
-            f.write('Item:')                
-            f.write(str(item))
-            f.write('\n')
-            f.write('Attributes of item:')
-            f.write(getattr(item, 'title_label'))                
-            f.write('\n')
-            for field in item:
-                f.write('Field ')
-                f.write(str(type(field)))
-                f.write(':')
-                f.write(str(field))
-                f.write('=')
-                value = item[field]
-                f.write(str(type(value)))
-                f.write(':')
-                f.write(str(value))
-                f.write('\n')
-                
-        f.close()
-        return item
-    
-    def open_spider(self, spider):
-        log.msg("[FileExporterPipeline] Open spider:" + spider.name, level=log.DEBUG)
-    
-    def close_spider(self, spider):
-        log.msg("[FileExporterPipeline] Close spider:" + spider.name, level=log.DEBUG)
