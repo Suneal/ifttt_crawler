dd5302956af99abddd3e6e2761a8eb7378d8e2bd
Miguel Coronado <miguelcb84@gmail.com>
2013-09-19 02:26:04 +0200

    Removed unusefull exporter

--- ifttt/rdf/jinja_exporters.py
+++ ifttt/rdf/jinja_exporters.py
@@ -11,47 +11,11 @@ import re
 from scrapy import log
 from scrapy.contrib.exporter import BaseItemExporter
 
-from ifttt.items import ChannelItem, RecipeItem
+from ifttt.items import RecipeItem
 from jinja2 import Environment, PackageLoader
 
 
-class JinjaExporter(BaseItemExporter):
-    ''' 
-    '''
-    def __init__(self, file, **kwargs):
-        # This should extract the name of the package automatically
-        self.env = Environment(loader=PackageLoader('ifttt', 'templates'))
-        self.file = file
-
-    def start_exporting(self):
-        pass
-    
-    def export_item(self, item):
-        ''' This method is called each time the scrapped_item signal is 
-            received (as usual). It is responsible of invoking the template 
-            renderer with the appropriate arguments.
-            
-            Args:
-                item(Item):    The item scraped            
-        '''
-        log.msg("[JinjaExporter] Exporting item " + str(item), level=log.DEBUG)
-        
-        if not item.template:
-            log.msg("No template attribute for item:" + str(item), level=log.WARNING)
-            return
-        
-        template = self.env.get_template(item.template + '.rdf')
-        log.msg("[JinjaExporter] Template loaded", level=log.DEBUG)        
-        out = template.render(item=item)
-        log.msg("[JinjaExporter] Template rendered", level=log.DEBUG)        
-        self.file.write(out)
-        
-    
-    def finish_exporting(self):
-        self.file.close()
-
-
-class JinjaExporterMultifile(JinjaExporter):
+class JinjaExporterMultifile(BaseItemExporter):
     ''' 
         Args:
             file(File) - a file
